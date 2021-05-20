7b707447bb95760b99803a5b2cdd313262f3669c
ghost <9334746+the-robot@users.noreply.github.com>
2021-05-20 22:55:38 +0800

    feat: more tools

---
+++ scripts/install-yay.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Constants
+readonly REPOSITORY_URL="https://aur.archlinux.org/yay.git"
+readonly REPOSITORY_DIR="yay-git"
+
+pacman -S --needed git base-devel
+
+git clone $REPOSITORY_URL /tmp/$REPOSITORY_DIR
+
+cd /tmp/$REPOSITORY_DIR
+
+makepkg -si
+
+cd /tmp
+
+rm -rf /tmp/$REPOSITORY_DIR
