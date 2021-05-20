190e280e2e228c79a33c02df9a795dfcf4bca918
ghost <9334746+the-robot@users.noreply.github.com>
2021-05-20 23:58:04 +0800

    debug

--- scripts/install-postman.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if ! command -v "postman" &> /dev/null
-then
-    yay -a -Sy --noconfirm --cleanafter postman-bin
-fi
--- scripts/install-spotify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-if ! command -v "spotify" &> /dev/null
-then
-    yay -a -Sy --noconfirm --cleanafter spotify
-fi
