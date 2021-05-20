a8e839b2992c988999acc2e963975829f664e2c8
ghost <9334746+the-robot@users.noreply.github.com>
2021-05-20 21:02:36 +0800

    refactor: cleanup installation code

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,15 @@
+function install_if_not_exists() {
+    if ! command -v "$1" &> /dev/null
+    then
+        echo "installing $1"
+        pacman -S --noconfirm $1
+    else
+        echo "$1 is already installed"
+    fi
+}
+
 # install git if not exists
-if ! command -v git &> /dev/null
-then
-    echo "Installing git"
-    pacman -S --noconfirm git
-fi
+install_if_not_exists "git"
 
 # install ansible if not exists
-if ! command -v ansible &> /dev/null
-then
-    echo "Installing ansible"
-    pacman -S --noconfirm ansible
-fi
+install_if_not_exists "ansible"
