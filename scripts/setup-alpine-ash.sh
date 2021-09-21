0a806136094360b937415c13b742b43bc61102a5
the-robot <9334746+the-robot@users.noreply.github.com>
2021-09-21 14:55:47 +0800

    Update setup-alpine-ash.sh

--- scripts/setup-alpine-ash.sh
+++ scripts/setup-alpine-ash.sh
@@ -8,3 +8,6 @@ apk add git python2 python3 python3-dev py3-pip nodejs-current npm go openssh tm
 
 # install extras
 apk add nmap netcat-openbsd openssh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
