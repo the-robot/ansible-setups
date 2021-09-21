3d0a8e615a8f79baf43c0ebbc7a8b55104a7b443
the-robot <9334746+the-robot@users.noreply.github.com>
2021-09-21 14:13:52 +0800

    Update setup-alpine.sh

--- scripts/setup-alpine.sh
+++ scripts/setup-alpine.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Constants
+readonly REPOSITORY_URL="https://github.com/the-robot/ansible-setups.git"
+readonly REPOSITORY_DIR="ansible-setups"
+
+# update packages
+apk update
+
+# install essential packages
+apk add git
+apk add ansible
+
+# pull ansible template
+ansible-pull -U $REPOSITORY_URL alpine.yaml
